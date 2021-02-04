12e9d9f0824582b5b866c6028f261240d1114c8a
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-02-03 22:17:56 -0300

    Entendendo mais sobre Describes e It

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,32 @@
+
+/// <reference types="cypress"/>
+
+it('Nome do teste que vai ser executado (Test Externo)', () => {
+
+})
+
+
+describe('Grupo 1 de varios test dentro desse Describe', () => {
+
+    it('Test interno 1', () => {
+
+    })
+
+    it('Test interno 2', () => {
+
+    })
+})
+
+describe(' Grupo 2 de varios test dentro desse Describe', () => {
+
+   describe('Outro Grupo de varios tests', () => {
+
+    it.skip('Test interno 1', () => {
+
+    })
+
+    it.only('Test interno 2', () => {
+
+    })
+})
+})
