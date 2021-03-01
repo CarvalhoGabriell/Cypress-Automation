a5ada7ca3c0fac0cdc643f65ed4842e646f7a3b5
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-02-28 22:23:26 -0300

    importando plugin do Xpath e adicionaod exeplo de preferencias de 'ID' no cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,17 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+require('cypress-xpath')
+/*
+Voce pode fazer uma lista com as prioridades com que o cypress ache os locators do HTML,
+Ou ate mesmo restringir nas tags que vcoce quer como prioridade. Além disso voce consegue adicionar
+Um locator que voce criou para fins de Test ou algo do tipo e que nao seja proprio do cypress e assim
+ele consegue indentificar.
+EXEMPLO ABAIXO:
+Cypress.SelectorPlayground.defaults({
+  selectorPriority: ['id', 'class', 'attributes']
+})
+*/
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
