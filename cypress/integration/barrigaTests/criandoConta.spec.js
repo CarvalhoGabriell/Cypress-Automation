92a90c324c144ce6ac7694dd10e9f9c7a3c4441b
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-03-20 19:46:46 -0300

    Novo cenario de test, agora testando a aplicação seubarrigareact

---
+++ cypress/integration/barrigaTests/criandoConta.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+import loc from '../../support/locators'
+import cmd from '../../support/commands'
+// GabsTest  1234
+describe('Fazendo o Login e validando mensagem de sucesso', () => {
+
+    before(() => {
+        cy.cmd.loginApp('gabTest@test.com', '1234')
+        cy.cmd.resetDados()
+    })
+
+    it('Inserir/ Criar uma conta.', () => {
+
+        cy.get(loc.MENU.SETTING).click()
+        cy.get(loc.MENU.CONTAS).click()
+        cy.get(loc.ADD_CONTA.NOME_CONTA).type('Conta nova')
+        cy.get(loc.ADD_CONTA.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
+    })
+
+    it('Alterando conta criada', () => {
+
+        cy.get(loc.MENU.SETTING).click()
+        cy.get(loc.MENU.CONTAS).click()
+        cy.xpath(loc.ADD_CONTA.XPATH_CONTA).click()
+        cy.get(loc.ADD_CONTA.NOME_CONTA).clear().type('Conta alterada')
+        cy.get(loc.ADD_CONTA.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
+
+    })
+})
